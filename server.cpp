61df8f46e44732f7bb2692c7397356046d50cb76
Kārlis Balodis-Bērziņš <karlis.berzins94@gmail.com>
2025-07-01 10:25:30 +0300

    i can send messages \o/

--- server.cpp
+++ server.cpp
@@ -6,6 +6,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#include <unistd.h>
+
 using namespace std;
 
 struct {
@@ -17,32 +19,52 @@ struct {
   string misc;
 } map_obj;
 
-bool close;
+bool running;
+string test;
 
 void login(int PORT){
+    // https://www.tutorialspoint.com/cplusplus/cpp_socket_programming.htm
     int opt = 1;
+    int a;
     int sockid = socket(AF_INET, SOCK_STREAM, 0);
+    //int new_socket;
     struct sockaddr_in in_addr;
+    int addr_len = sizeof(in_addr);
+    char buffer[1024] = {0};
 
     setsockopt(sockid, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
     in_addr.sin_family = AF_INET;
     in_addr.sin_addr.s_addr = INADDR_ANY;
     in_addr.sin_port = htons(PORT);
     bind(sockid, (struct sockaddr *)&in_addr, sizeof(in_addr));
-
-
+    //while(running){
+        if (listen(sockid, 3) >= 0) {
+            a = accept(sockid, (struct sockaddr *)&in_addr, (socklen_t *)&addr_len);
+        }
+        if (a>0){
+            mvprintw(1,1,"listening");
+        }
+        read(a, buffer, 1024);
+        mvprintw(2,2,buffer);
+        close(a);
+        this_thread::sleep_for(chrono::milliseconds(10));
+    //}
+    close(sockid);
+    test = buffer;
 }
 
 int main() {
-    close = false;
+    running = true;
     char ch;
     WINDOW* win = initscr();
     noecho();
     nodelay(win, true);
+    thread join(login, 1234);
     while(true){
         this_thread::sleep_for(chrono::milliseconds(10));
         ch = getch();
         if (ch == 'q'){
+            running = false;
             break;
         }else if (ch != -1){
             mvaddch(5,5,'[');
@@ -50,9 +72,11 @@ int main() {
             addch(']');
         }
     }
+    join.join();
     mvprintw(5,5, "AAAAAA");
     nodelay(win, false);
     echo();
     endwin();
+    cout << test;
     return 0;
 }
